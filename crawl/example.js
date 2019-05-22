faf2fcd23a1c03dfd35e65ebabc683a1cce8e8ae
Thorge Petersen <thorgepetersen@gmail.com>
2019-05-22 11:09:18 +0200

    fs write for example

--- crawl/example.js
+++ crawl/example.js
@@ -1,5 +1,17 @@
 const crawl = require('./crawl');
+const fs = require('fs');
+
 
 crawl.retrieve('Albert Einstein', function(body) {
-  console.log(body.results.bindings);
+
+  
+  
+  fs.writeFile("./result_"+ Date.now() +".json", JSON.stringify(body.results.bindings), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+  }); 
+
 });
